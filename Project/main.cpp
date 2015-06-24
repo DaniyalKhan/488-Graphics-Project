f3139648190cd36e24a08d8d022756f0c714f1eb
Daniyal <d.muhammad.khan@gmail.com>
2015-06-24 13:22:41 -0400

    Initial Commit

---
+++ Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project
+//
+//  Created by Daniyal Khan on 2015-06-24.
+//  Copyright (c) 2015 Daniyal Khan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
