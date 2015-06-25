5b6cc7db45bf89d302d10986ce7337f9f9a417b8
Daniyal <d.muhammad.khan@gmail.com>
2015-06-24 20:21:54 -0400

    adding shader loading, xcode project files

---
+++ Shaders/Loader.h
@@ -0,0 +1,20 @@
+//
+//  Loader.h
+//  Project
+//
+//  Created by Daniyal Khan on 2015-06-24.
+//  Copyright (c) 2015 Daniyal Khan. All rights reserved.
+//
+
+#ifndef __Project__Loader__
+#define __Project__Loader__
+
+#include <stdio.h>
+
+#include "glew.h"
+
+GLuint LoadShader(const char *vertex_path, const char *fragment_path);
+
+
+#endif /* defined(__Project__Loader__) */
+
